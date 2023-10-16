345b9b0f481c51e78ac4ff9ae99da31280e2753a
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-10-16 15:45:13 +0200

    [ add ] method for language display

--- src/components/partials/CardContainer.vue
+++ src/components/partials/CardContainer.vue
@@ -8,6 +8,13 @@ export default {
       store
     }
   },
+  methods:{
+    checkLanguage(value){
+      if (value.original_language != 'en' && value.original_language != 'it'){
+        return value.original_language
+      }
+    }
+  }
 }
 </script>
 
@@ -17,7 +24,9 @@ export default {
       <li v-for="film in store.listFilm" :key="film.id">
         <h3>{{ film.title }}</h3>
         <h4>{{ film.original_title }}</h4> 
-        <p>{{ film.original_language }}</p>
+        <img v-if="film.original_language.includes('en')" src="/public/img/en.png" alt="en">
+        <img v-if="film.original_language.includes('it')" src="/public/img/it.png" alt="it">
+        <p> {{ checkLanguage(film) }}</p>
         <p>{{ film.vote_average }}</p>
       </li>
     </ul>
@@ -27,8 +36,8 @@ export default {
 
 <style lang="scss" scoped>
 
-  .input-group {
-    width: 200px;
+  img {
+    width: 30px;
   }
 
 
