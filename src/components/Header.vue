36e126ded859ec0d14cbf11af1531c7bb99d7791
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-10-16 12:38:50 +0200

    [ init ] components

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+  name:'Header'
+}
+</script>
+
+
+<template>
+  <header class="d-flex align-items-center" >
+    <img src="/public/img/logo-boolflix.png" alt="">
+  </header>
+</template>
+
+
+
+<style lang="scss" scoped>
+@use '../scss/main' as *;
+
+  header{
+    width: 100%;
+    height: 60px;
+    padding-left: 20px;
+    background-color: white;
+    img {
+      width: 70px;
+      height: 24px;
+      margin-right: 10px;
+    }
+  }
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name:'Main',
+}
+</script>
+
+
+<template>
+  <main>
+    <h2>main</h2>
+  </main>
+</template>
+
+
+
+<style lang="scss" scoped>
+
+</style>
