1718162f171d74791962ebbbd681109ac4d49389
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-10-18 11:58:17 +0200

    [ add ] rated film array

--- src/data/store.js
+++ src/data/store.js
@@ -2,11 +2,13 @@ import { reactive } from 'vue';
 
 export const store = reactive({
   apiUrl:'https://api.themoviedb.org/3/search/',
+  apiTrendUrl:'https://api.themoviedb.org/3/trending/movie/week',
   api_key:'70e1fcb24d9cf905714f0cb390e8861a',
   language:'it-IT',
   imgPath:'http://image.tmdb.org/t/p/w342/',
   search: '',
   option:'',
+  trendMovie:[],
   movie:[],
   tv:[]
 })
